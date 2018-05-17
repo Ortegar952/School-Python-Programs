2ebeb875f2c395ca0058f5adec6da7cbb1d9e072
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-05-17 11:33:18 -0400

    X on Turtle

---
+++ x-on-turtle.py
@@ -0,0 +1,30 @@
+#Tommy Garcia
+# X on Turtle
+
+import turtle
+
+
+def main():
+
+    alex=turtle.Turtle()
+
+    alex.hideturtle()
+
+    for i in range(4):
+        alex.left(45)
+        alex.forward(50)
+        alex.penup()
+        alex.goto(0,0)
+        alex.left(45)
+        alex.pendown()
+        
+
+
+
+
+
+
+
+
+main()
+    
