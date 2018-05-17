da314c5658baeff3e562ab562e315b7b472c49fa
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-05-17 11:02:48 -0400

    Converts kilometers to miles

---
+++ km2miles.py
@@ -0,0 +1,10 @@
+#Tommy Garcia
+#Program converts kilometers to miles
+
+def main():
+    km=eval(input("Enter distance in Kilimeters: "))
+    miles=0.621371*km
+    print("The distance in miles is", miles)
+
+main()
+            
