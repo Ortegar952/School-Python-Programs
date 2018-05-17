a8acfeab6a86afe4e176039c35edd7d762b55b59
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-05-17 10:44:28 -0400

    This program tells you the year you were born from your current age

---
+++ Birthyear.py
@@ -0,0 +1,11 @@
+#Tommy Garcia
+#This program tells you the year you were born from your current age
+
+def main():
+    age=eval(input("What is your age in 2018?"))
+    birthyear= 2018-age
+    print("You were born in the year",birthyear,)
+    if age>40:
+        print("\nYou old geezer!!!!")
+    
+main()
