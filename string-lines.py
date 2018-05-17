4727bbeb573671a4de6f0c22bea675f5b166e0a5
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-05-17 11:31:59 -0400

    Prints a user-entered string into individual lines

---
+++ string-lines.py
@@ -0,0 +1,19 @@
+#Tommy Garcia
+#Prints a user-entered string into individual lines
+
+def main():
+
+    Y_Str=input("Enter a string: ")
+    Y_Str[:]     
+    for a in Y_Str:
+        print(a)
+
+
+
+
+
+
+
+
+
+main()
