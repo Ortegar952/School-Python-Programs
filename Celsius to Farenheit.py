5f2f4a849c750c8e5049f224c7377ecbd6b454e4
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-05-17 11:01:52 -0400

    Converts temperature from Celsius to Farenheit

---
+++ Celsius to Farenheit.py
@@ -0,0 +1,9 @@
+#Tommy Garcia
+#This program converts temperature from Celsius to Farenheit
+
+def main():
+    celsius=eval(input("What is the Celsius temperature? "))
+    farenheit= (9/5)*celsius+32
+    print("The temperature is",farenheit,"degrees Farenheit.")
+
+main()
