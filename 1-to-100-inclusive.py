932404e380c56adbdd9e3be83c03bb8a74f16d86
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-05-17 11:29:36 -0400

    Program to add the numbers 1 to 100 inclusive.

---
+++ 1-to-100-inclusive.py
@@ -0,0 +1,11 @@
+#Tommy Garcia
+#Program to add the numbers 1 to 100 inclusive.
+
+def main():
+    runningSum=0
+    for i in range(101):
+        runningSum=runningSum+i
+    print("The total is",runningSum)
+
+main()
+        
