07eb0063f9b1bf54aee4a22328a0c15ad5785abb
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-05-17 11:32:47 -0400

    Program asks the user for a string and then prints it out +#in all upper case and then all lower case.

---
+++ uppercase-and-lowercase-strings.py
@@ -0,0 +1,14 @@
+#Tommy Garcia
+#Program asks the user for a string and then prints it out
+#in all upper case and then all lower case.
+
+
+def main():
+    
+
+    String=input("Please enter a string: ")
+    print("You entered(shouting):",String.upper()+"!")
+    print("You entered (whispering):",String.lower())
+
+
+main()
